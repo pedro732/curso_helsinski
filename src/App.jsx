5cacc08d7b16e3f3098ef9eeda13adca8d5a5fb5
pedroandrade_13 <pedroandrade@live.cl>
2023-12-14 14:45:49 -0300

    tareas 1.1 y 1.2

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+
+
+const Header=()=>{
+  return(
+    <div>
+      <h1>Half Stack application development</h1>
+    </div>
+  )
+
+}
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header />
+      <Content parts={parts} />
+     
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+
+export default App
