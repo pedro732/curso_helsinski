1c65de35c7c20cc721d3e75d91a51844d746cef8
pedroandrade_13 <pedroandrade@live.cl>
2023-12-14 15:59:24 -0300

    Tarea 1.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  return (
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return(
     <div>
-      <h1>{course}</h1>
-      {parts.map(({name, exercises}, index)=>(
-        <p key={index}>{name} {exercises}</p>
-      ))}
+      <h1>{course.name}</h1>
+      
+        {course.parts.map(({name,exercises},index)=>
+          <p key={index}>{name} {exercises}</p>
+        )}
+     
     </div>
   )
 }
